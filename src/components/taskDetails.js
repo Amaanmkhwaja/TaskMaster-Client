65663c5201cd45faec86f29bff2b5c161fdd16a6
Amaanmkhwaja <amaanmkhwaja@gmail.com>
2023-04-12 15:55:44 -0400

    changed to localhost route

--- src/components/taskDetails.js
+++ src/components/taskDetails.js
@@ -13,9 +13,9 @@ function TaskDetails({task}) {
 
     try {
       // localhost
-      // const response = await fetch('/api/tasks/' + task._id, {
+      const response = await fetch('/api/tasks/' + task._id, {
       //deployed
-      const response = await fetch('https://taskmaster-server.herokuapp.com/api/tasks' + task._id, {
+      // const response = await fetch('https://taskmaster-server.herokuapp.com/api/tasks' + task._id, {
         method: 'DELETE'
       })
       const json = await response.json()
