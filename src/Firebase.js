6f6f0fead96bded4138244a43fa025b78a930060
Amaanmkhwaja <amaanmkhwaja@gmail.com>
2023-04-27 17:15:45 -0400

    hardcoded firebase config instead of process.env

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 // import { useNavigate } from 'react-router-dom';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDov0kus1wHG_Vo5Fv2r614RDHskaEtJTY",
+  authDomain: "taskmaster-15fac.firebaseapp.com",
+  projectId: "taskmaster-15fac",
+  storageBucket: "taskmaster-15fac.appspot.com",
+  messagingSenderId: "961343730815",
+  appId: "1:961343730815:web:e195a613786607bbeabb4a",
 };
 
 const app = initializeApp(firebaseConfig);
